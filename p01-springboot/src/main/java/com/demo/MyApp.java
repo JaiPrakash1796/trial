ede08bc7fbe7620e72f92b7d5ec7be252bcf7e37
2030162 <2030162@LTIN294616.cts.com>
2021-08-04 14:35:57 +0530

    1

---
+++ p01-springboot/src/main/java/com/demo/MyApp.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApp  {
+	public static void main(String[] args) {
+		
+			SpringApplication.run(MyApp.class, args);
+
+			System.out.println("hi");
+	}
+
+
+	
+}
